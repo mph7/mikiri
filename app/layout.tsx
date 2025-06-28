bc44a50c43dc2dafc0cfa9d1d2accc36a35252e4
mph7 <matheus.philippe2002@gmail.com>
2025-06-28 18:51:03 -0300

    feat: implement login page layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="w-dvw h-dvh">
+          {children}
+        </div>
       </body>
     </html>
   );
